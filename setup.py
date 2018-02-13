83a171df1bb8ac44026e47441f2cb0194a0f4372
Sumer Jabri <sumer.jabri@craftercms.org>
2018-02-13 16:56:11 -0500

    Updated version number, license and fixed some config.

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ from setuptools import setup
 setup(
     name='craftercms_sphinx_theme',
     description='Crafter CMS Docs theme for Sphinx Doc',
-    version='1.0.0',
+    version='1.1.4',
     url='https://github.com/craftercms/craftercms-sphinx-theme/',
-    license='GPL 3.0',
+    license='GPL version 3',
     author='Roy Art',
     author_email='royart@me.com',
-    long_description=open('README.rst', encoding='utf-8').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     zip_safe=False,
     packages=['craftercms_sphinx_theme'],
     package_data={
@@ -42,23 +42,17 @@ setup(
         #   4 - Beta
         #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        'License :: OSI Approved :: GPL 3.0',
-
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Environment :: Console',
         'Environment :: Web Environment',
-
         'Intended Audience :: Developers',
-
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-
         'Operating System :: OS Independent',
-
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
     ]
